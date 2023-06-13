ff815ec4642a2dae8949dbb6a8c3a8564cde6487
Sophie McNally <soph_mcnally@msn.com>
2023-06-13 16:21:49 +0100

    adding contact information to contact.js

---
+++ my-app/src/components/pages/contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Email, GitHub, LinkedIn } from '@mui/icons-material';
+import '../../App.css';
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <h2 className="contact-heading">Contact Me</h2>
+      <div className="contact-icons">
+        <a href="mailto:soph_mcnally@msn.com">
+          <Email sx={{ fontSize: 50 }} className="icon" />
+        </a>
+        <a href="https://github.com/SophieEmsi">
+          <GitHub sx={{ fontSize: 50 }} className="icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/your-linkedin-username">
+          <LinkedIn sx={{ fontSize: 50 }} className="icon" />
+        </a>
+      </div>
+      <p className="contact-description">
+        If you would like to discuss any of my projects or job opportunities, feel free to reach out to me via email,
+        GitHub, or LinkedIn.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
