5450296dac1fdb90801079f3875fbbde76e80401
Sophie McNally <soph_mcnally@msn.com>
2023-06-14 14:51:02 +0100

    adding center banner to page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,39 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
+import './App.css';
 
 import About from './components/pages/about';
 import Experience from './components/pages/experience';
 import Portfolio from './components/pages/portfolio';
 import Contact from './components/pages/contact';
 
+import bannerImage from './components/assets/Banner.png';
+
 const App = () => {
   return (
     <Router>
-      <div style={{ display: 'flex', height: '100vh' }}>
+      <div className="app-container">
         {/* Left Side */}
-        <div
-          style={{
-            flex: 0.75,
-            width: '300px',
-            backgroundColor: '#1e1e1e',
-            overflow: 'hidden',
-            paddingTop: '80px',
-            borderRight: '1px solid black',
-            padding: '50px',
-          }}
-        >
-          <header style={{ marginBottom: '80px', marginTop: '50px', color: '#3fcbd1' }}>
-    <h1>Sophie McNally</h1>
-    <h2>Front-End Web Developer</h2>
-    <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
-  </header>
+        <div className="left-side">
+          <header>
+            <h1>Sophie McNally</h1>
+            <h2>Front-End Web Developer</h2>
+            <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
+          </header>
           <Nav defaultActiveKey="/" className="flex-column">
-
-            <Nav.Link as={Link} to="/about" eventKey="/about" className="nav-link" style={{ marginBottom: '10px', color: '#3fcbd1' }}>About</Nav.Link>
-            <Nav.Link as={Link} to="/experience" eventKey="/experience" className="nav-link" style={{ marginBottom: '10px', color: '#3fcbd1' }}>Experience</Nav.Link>
-            <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio" className="nav-link" style={{ marginBottom: '10px', color: '#3fcbd1' }}>Portfolio</Nav.Link>
-            <Nav.Link as={Link} to="/contact" eventKey="/contact" className="nav-link" style={{ marginBottom: '10px', color: '#3fcbd1' }}>Contact</Nav.Link>
+            <Nav.Link as={Link} to="/about" eventKey="/about" className="nav-link">About</Nav.Link>
+            <Nav.Link as={Link} to="/experience" eventKey="/experience" className="nav-link">Experience</Nav.Link>
+            <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio" className="nav-link">Portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/contact" eventKey="/contact" className="nav-link">Contact</Nav.Link>
           </Nav>
         </div>
+        <div className="center-image">
+          <img src={bannerImage} alt="Image" className="custom-image" />
+        </div>
         {/* Right Side */}
-        <div
-          style={{
-            flex: 1,
-            backgroundColor: 'white',
-            overflowY: 'scroll',
-            padding: '50px',
-            marginBottom: '80px',
-            marginTop: '50px',
-          }}
-        >
+        <div className="right-side">
           <Routes>
-          
             <Route path="/about" element={<About />} />
             <Route path="/experience" element={<Experience />} />
             <Route path="/portfolio" element={<Portfolio />} />
