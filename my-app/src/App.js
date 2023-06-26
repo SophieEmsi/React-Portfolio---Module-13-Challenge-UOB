00412493566b4ad28c5ecac6410528ba5efaa7fb
Sophie McNally <soph_mcnally@msn.com>
2023-06-26 20:00:29 +0100

    Editing the page design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,120 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import { Nav } from 'react-bootstrap';
+// import './App.css';
+
+// import About from './components/pages/about';
+// import Experience from './components/pages/experience';
+// import Portfolio from './components/pages/portfolio';
+// import Contact from './components/pages/contact';
+
+// import bannerImage from './components/assets/Banner.png';
+// import avatar from './components/assets/Avatar.png';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div className="app-container">
+//         {/* Left Side */}
+//         <div className="left-side">
+//           <header>
+//             <h1>Sophie McNally</h1>
+//             <h2>Front-End Web Developer</h2>
+//             <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
+//           </header>
+//           <Nav defaultActiveKey="/" className="flex-column">
+//             <Nav.Link as={Link} to="/about" eventKey="/about" className="nav-link">About</Nav.Link>
+//             <Nav.Link as={Link} to="/experience" eventKey="/experience" className="nav-link">Experience</Nav.Link>
+//             <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio" className="nav-link">Portfolio</Nav.Link>
+//             <Nav.Link as={Link} to="/contact" eventKey="/contact" className="nav-link">Contact</Nav.Link>
+//           </Nav>
+//         </div>
+//         <div className="center-container">
+//           <div className="center-image">
+//             <img src={bannerImage} alt="Image" className="custom-image" />
+//           </div>
+//         </div>
+//         {/* Right Side */}
+//         <div className="right-side">
+//           <Routes>
+//             <Route path="/about" element={<About />} />
+//             <Route path="/experience" element={<Experience />} />
+//             <Route path="/portfolio" element={<Portfolio />} />
+//             <Route path="/contact" element={<Contact />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/pages/navbar.js';
 
-import About from './components/pages/about';
-import Experience from './components/pages/experience';
-import Portfolio from './components/pages/portfolio';
-import Contact from './components/pages/contact';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSection: 'aboutSection',
+    };
+  }
 
-import bannerImage from './components/assets/Banner.png';
-import avatar from './components/assets/Avatar.png';
+  handleNavClick = (sectionId) => {
+    this.setState({ activeSection: sectionId });
+  };
 
-const App = () => {
-  return (
-    <Router>
-      <div className="app-container">
-        {/* Left Side */}
-        <div className="left-side">
-          <header>
-            <h1>Sophie McNally</h1>
-            <h2>Front-End Web Developer</h2>
-            <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
-          </header>
-          <Nav defaultActiveKey="/" className="flex-column">
-            <Nav.Link as={Link} to="/about" eventKey="/about" className="nav-link">About</Nav.Link>
-            <Nav.Link as={Link} to="/experience" eventKey="/experience" className="nav-link">Experience</Nav.Link>
-            <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio" className="nav-link">Portfolio</Nav.Link>
-            <Nav.Link as={Link} to="/contact" eventKey="/contact" className="nav-link">Contact</Nav.Link>
-          </Nav>
+  renderSection = (sectionId, title, content) => {
+    return (
+      <section
+        id={sectionId}
+        className={this.state.activeSection === sectionId ? 'bg-light' : ''}
+      >
+        <div className="container">
+          <h2>{title}</h2>
+          <p>{content}</p>
         </div>
-        <div className="center-container">
-          <div className="center-image">
-            <img src={bannerImage} alt="Image" className="custom-image" />
+      </section>
+    );
+  };
+
+  render() {
+    const headings = [
+      { id: 'aboutSection', title: 'About Me' },
+      { id: 'contactSection', title: 'Contact' },
+      { id: 'experienceSection', title: 'Experience' },
+      { id: 'portfolioSection', title: 'Portfolio' },
+    ];
+
+    return (
+      <div>
+        <Navbar
+          headings={headings}
+          activeSection={this.state.activeSection}
+          handleNavClick={this.handleNavClick}
+        />
+
+        {/* Jumbotron */}
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Welcome to my website!</h1>
+            <p>
+              This is the main content area where the selected section
+              information will be displayed.
+            </p>
           </div>
         </div>
-        {/* Right Side */}
-        <div className="right-side">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+
+        {/* Sections */}
+        {this.renderSection('aboutSection', 'About Me', 'This is the content of section 1.')}
+        {this.renderSection('contactSection', 'Contact', 'This is the content of section 2.')}
+        {this.renderSection('experienceSection', 'Experience', 'This is the content of section 3.')}
+        {this.renderSection('portfolioSection', 'Portfolio', 'This is the content of section 4.')}
       </div>
-    </Router>
-  );
-};
+    );
+  }
+}
 
 export default App;
---
+++ my-app/src/components/pages/navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    const { headings, activeSection, handleNavClick } = this.props;
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Website
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {headings.map((heading, index) => (
+              <li className="nav-item" key={index}>
+                <a
+                  className={`nav-link ${
+                    activeSection === heading.id ? 'active' : ''
+                  }`}
+                  href={`#${heading.id}`}
+                  onClick={() => handleNavClick(heading.id)}
+                >
+                  {heading.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
