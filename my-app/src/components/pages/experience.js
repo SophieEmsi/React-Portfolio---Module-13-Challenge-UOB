434f1c669951ae607ff6f0e6e520074a9adbffe2
Sophie McNally <soph_mcnally@msn.com>
2023-06-29 19:11:45 +0100

    styling the Experience page

--- my-app/src/components/pages/experience.js
+++ my-app/src/components/pages/experience.js
@@ -2,53 +2,107 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../App.css';
 
+const Skills = () => (
+  <div className="right-column">
+  <h3 style={{ marginBottom: '25px', marginTop: '10px' }}>
+  <span className="heading-box">Skills</span>
+</h3>
+    <div className="skills-container">
+      <div className="skill-category">
+        <h4>Languages</h4>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>ES6</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Libraries</h4>
+        <ul>
+          <li>React.js</li>
+          <li>jQuery</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Frameworks</h4>
+        <ul>
+          <li>Node.js</li>
+          <li>Bootstrap</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Testing</h4>
+        <ul>
+          <li>Jest</li>
+          <li>Insomnia REST</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Design</h4>
+        <ul>
+          <li>Figma</li>
+          <li>User Experience Design</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Programming Interface</h4>
+        <ul>
+          <li>The DOM</li>
+          <li>APIs</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Data Format</h4>
+        <ul>
+          <li>JSON</li>
+        </ul>
+      </div>
+      <div className="skill-category">
+        <h4>Programming Technique</h4>
+        <ul>
+          <li>AJAX</li>
+          <li>Object Oriented Programming (OOP)</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
 const Experience = () => {
   return (
-    <div>
-      <h3 style={{ marginBottom: '25px', marginTop: '10px' }}>Education</h3>
-      <div className="education-item">
-        <div className="date">Sept 2010 - June 2013</div>
-        <div className="description">
-          <h3>Nottingham Trent University</h3>
-          <p>2:1 - BA (Hons) Marketing, Design & Communication</p>
+    <div className="experience-page">
+      <div className="left-column">
+      <h3 style={{ marginBottom: '25px', marginTop: '10px' }}>
+          <span className="heading-box">Education</span>
+      </h3>
+        <div className="education-item">
+          <div className="date">Sept 2010 - June 2013</div>
+          <div className="description">
+            <h3>Nottingham Trent University</h3>
+            <p>2:1 - BA (Hons) Marketing, Design & Communication</p>
+          </div>
         </div>
-      </div>
-      <div className="education-item">
-        <div className="date">Feb 2023 - June 2023</div>
-        <div className="description">
-          <h3>University of Birmingham</h3>
-          <p>Front-End Web Development Bootcamp</p>
+        <div className="education-item">
+          <div className="date">Feb 2023 - June 2023</div>
+          <div className="description">
+            <h3>University of Birmingham</h3>
+            <p>Front-End Web Development Bootcamp</p>
+          </div>
         </div>
-      </div>
-
-      <h3 style={{ marginBottom: '25px' }}>Experience</h3>
-      <div className="experience-item">
-        <div className="date">March 2016 - Present</div>
-        <div className="description">
-          <h3>HM Revenue & Customs</h3>
+        <div>
+        <h3 style={{ marginBottom: '25px', marginTop: '10px' }}>
+            <span className="heading-box">Full CV</span>
+        </h3>
           <p>
-            In my role as a member of the User Experience team in commercial 
-            building design projects, I actively contribute to enhancing the 
-            user experience of office buildings. I am responsible for gathering 
-            and analyzing valuable user feedback data, gaining deep insights 
-            into the needs and preferences of occupants. With a strong focus on 
-            inclusivity, I ensure that the buildings are designed to accommodate 
-            and embrace diversities. By incorporating universal design principles 
-            and considering the unique requirements of various individuals, I 
-            collaborate with cross-functional teams to refine and improve the 
-            design of office spaces.
+            For my complete work history and qualifications, please refer to my{' '}
+            <a href={require('../assets/CV - Sophie McNally.docx.pdf')} target="_blank" rel="noopener noreferrer">
+              Full CV
+            </a>
           </p>
         </div>
       </div>
-      
-      <div>
-        <h3 style={{ marginBottom: '25px' }}>Full CV</h3>
-        <p>
-        For my complete work history and qualifications, please refer to my{' '}
-          <a href={require('../assets/CV - Sophie McNally.docx.pdf')} target="_blank" rel="noopener noreferrer">Full CV</a>
-
-        </p>
-      </div>
+      <Skills />
     </div>
   );
 };
