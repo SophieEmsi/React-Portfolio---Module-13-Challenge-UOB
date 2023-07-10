6807b7574bec04c301dc621d826212d8931ae10d
Sophie McNally <soph_mcnally@msn.com>
2023-07-10 17:51:14 +0100

    updating project cards with images and links

--- my-app/src/components/pages/portfolio.js
+++ my-app/src/components/pages/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap';
 import myImage1 from '../assets/Project 1.png';
 import myImage2 from '../assets/Project 2.png';
 import myImage3 from '../assets/Project 3.png';
@@ -10,79 +11,74 @@ import myImage6 from '../assets/Project 6.png';
 const Portfolio = () => {
   const projects = [
     {
-      id: 6,
-      title: 'Edamam API Recipe Website',
-      image: myImage6,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url: 'https://sophieemsi.github.io/Edamam-Recipe-Website-Group-1/',
-      url2: 'https://github.com/SophieEmsi/Edamam-Recipe-Website-Group-1'
-    },
-    {
       id: 1,
-      title: 'Team Profile Generator',
+      title: 'Ecommerce Website',
       image: myImage1,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url: 'https://sophieemsi.github.io/Team-Profile-Generator---Module-12-Challenge/',
+      url: 'https://levels-e-commerce.netlify.app/',
+      url2: 'https://github.com/SophieEmsi/E-commerce-Website'
+    },
+    {
+      id: 2,
+      title: 'API Recipe Website',
+      image: myImage2,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      url: 'https://sophieemsi.github.io/Edamam-Recipe-Website/',
+      url2: 'https://github.com/SophieEmsi/Edamam-Recipe-Website'
+    },
+    {
+      id: 3,
+      title: 'Node.js Team Profile Generator',
+      image: myImage3,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      url: 'https://github.com/SophieEmsi/Team-Profile-Generator---Module-12-Challenge/blob/main/Output/team.html',
       url2: 'https://github.com/SophieEmsi/Team-Profile-Generator---Module-12-Challenge'
     },
     {
-      id: 1,
-      title: 'README Generator',
-      image: myImage2,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url: 'https://github.com/SophieEmsi/README-Generator---Module-11-Challenge/blob/main/README.md',
-      url2: 'https://github.com/SophieEmsi/README-Generator---Module-11-Challenge'
-    },
-    {
-      id: 1,
+      id: 4,
       title: 'Weather Dashboard',
-      image: myImage3,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url: 'https://sophieemsi.github.io/Weather-Dashboard---Module-8-Challenge/',
-      url2: 'https://github.com/SophieEmsi/Weather-Dashboard---Module-8-Challenge'
-
-    },
-    {
-      id: 1,
-      title: 'Work Day Scheduler',
       image: myImage4,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url: 'https://sophieemsi.github.io/Work-Day-Scheduler---Week-7-Challenge/',
-      url2: 'https://github.com/SophieEmsi/Work-Day-Scheduler---Week-7-Challenge'
+      url: 'https://sophieemsi.github.io/Weather-Dashboard---Module-8-Challenge',
+      url2: 'https://github.com/SophieEmsi/Weather-Dashboard---Module-8-Challenge'
     },
     {
-      id: 1,
-      title: 'Code Quiz',
+      id: 5,
+      title: 'Timed Quiz Challenge',
       image: myImage5,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       url: 'https://sophieemsi.github.io/Code-Quiz---Module-6-Challenge/',
       url2: 'https://github.com/SophieEmsi/Code-Quiz---Module-6-Challenge'
     },
+    {
+      id: 6,
+      title: 'Password Generator',
+      image: myImage6,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      url: 'https://sophieemsi.github.io/Password-Generator---Module-5-Challenge/',
+      url2: 'https://github.com/SophieEmsi/Password-Generator---Module-5-Challenge'
+    }
   ];
 
   return (
-    <div>
-      <h3 style={{ marginBottom: '25px', marginTop: '10px', textAlign: 'center' }}>
-        <span className="heading-box">Portfolio</span>
-      </h3>
-
-      <div className="portfolio-container">
-        {projects.map((project, index) => (
-          <div key={project.id} className="portfolio-item">
-            <div className="thumbnail-container">
-              <img src={project.image} alt={project.title} className="portfolio-thumbnail" />
+    <div className="d-flex flex-wrap justify-content-between">
+      {projects.map((project) => (
+        <Card className="portfolio-card mb-4" key={project.id}>
+          <Card.Img variant="top" src={project.image} className="carousel-image" />
+          <Card.Body>
+            <Card.Title>{project.title}</Card.Title>
+            <Card.Text>{project.description}</Card.Text>
+            <div className="carousel-links">
+              <Button href={project.url} target="_blank" rel="noopener noreferrer" variant="primary">
+                View Project
+              </Button>
+              <Button href={project.url2} target="_blank" rel="noopener noreferrer" variant="primary">
+                View Repo
+              </Button>
             </div>
-            <div className="portfolio-description">
-              <h4>{project.title}</h4>
-              <p>{project.description}</p>
-              <div className="portfolio-links">
-                <a href={project.url} target="_blank" rel="noopener noreferrer" className="portfolio-link">View Project</a>
-                <a href={project.url2} target="_blank" rel="noopener noreferrer" className="portfolio-link">View Repo</a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 };
