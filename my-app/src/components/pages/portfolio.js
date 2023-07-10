aeeb061eeeaaee2e21004ca82b93850f8d1aefe5
Sophie McNally <soph_mcnally@msn.com>
2023-07-10 21:17:34 +0100

    styling the projects buttons

--- my-app/src/components/pages/portfolio.js
+++ my-app/src/components/pages/portfolio.js
@@ -14,7 +14,7 @@ const Portfolio = () => {
       id: 1,
       title: 'Ecommerce Website',
       image: myImage1,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'An E-commerce Website built with Fake Store API',
       url: 'https://levels-e-commerce.netlify.app/',
       url2: 'https://github.com/SophieEmsi/E-commerce-Website'
     },
@@ -22,7 +22,7 @@ const Portfolio = () => {
       id: 2,
       title: 'API Recipe Website',
       image: myImage2,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'A recipe website built with nutrition in mind and using multiple APIs .',
       url: 'https://sophieemsi.github.io/Edamam-Recipe-Website/',
       url2: 'https://github.com/SophieEmsi/Edamam-Recipe-Website'
     },
@@ -30,7 +30,7 @@ const Portfolio = () => {
       id: 3,
       title: 'Node.js Team Profile Generator',
       image: myImage3,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'A team profile generater which is created using Node.js in VS Code',
       url: 'https://github.com/SophieEmsi/Team-Profile-Generator---Module-12-Challenge/blob/main/Output/team.html',
       url2: 'https://github.com/SophieEmsi/Team-Profile-Generator---Module-12-Challenge'
     },
@@ -38,7 +38,7 @@ const Portfolio = () => {
       id: 4,
       title: 'Weather Dashboard',
       image: myImage4,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'A weather dashboard created using a live weather API',
       url: 'https://sophieemsi.github.io/Weather-Dashboard---Module-8-Challenge',
       url2: 'https://github.com/SophieEmsi/Weather-Dashboard---Module-8-Challenge'
     },
@@ -46,7 +46,7 @@ const Portfolio = () => {
       id: 5,
       title: 'Timed Quiz Challenge',
       image: myImage5,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'A timed quiz of multiple choice questions created using Javascript',
       url: 'https://sophieemsi.github.io/Code-Quiz---Module-6-Challenge/',
       url2: 'https://github.com/SophieEmsi/Code-Quiz---Module-6-Challenge'
     },
@@ -54,7 +54,7 @@ const Portfolio = () => {
       id: 6,
       title: 'Password Generator',
       image: myImage6,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'A password generator created using Javascript',
       url: 'https://sophieemsi.github.io/Password-Generator---Module-5-Challenge/',
       url2: 'https://github.com/SophieEmsi/Password-Generator---Module-5-Challenge'
     }
@@ -64,15 +64,15 @@ const Portfolio = () => {
     <div className="d-flex flex-wrap justify-content-between">
       {projects.map((project) => (
         <Card className="portfolio-card mb-4" key={project.id}>
-          <Card.Img variant="top" src={project.image} className="carousel-image" />
+          <Card.Img variant="top" src={project.image} className="project-image" />
           <Card.Body>
             <Card.Title>{project.title}</Card.Title>
             <Card.Text>{project.description}</Card.Text>
-            <div className="carousel-links">
-              <Button href={project.url} target="_blank" rel="noopener noreferrer" variant="primary">
+            <div className="project-links">
+              <Button className="custom-button" href={project.url} target="_blank" rel="noopener noreferrer" variant="primary">
                 View Project
               </Button>
-              <Button href={project.url2} target="_blank" rel="noopener noreferrer" variant="primary">
+              <Button className="custom-button" href={project.url2} target="_blank" rel="noopener noreferrer" variant="primary">
                 View Repo
               </Button>
             </div>
